569f4d0b0ab6f5986a26dbbd38b1f5ae744992bf
gragard <trakseel0@gmail.com>
2026-07-17 11:35:56 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 window.SITE_CONFIG = {
   supabase: {
     url: "https://vynhivvyhzwipkiombxa.supabase.co",
-    anonKey: "sb_publishable_un_jxZbuWpXW-tLAUA6Lkg_RfTyb7a_"
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5bmhpdnZ5aHp3aXBraW9tYnhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxOTgwMjksImV4cCI6MjA5OTc3NDAyOX0.YBdxPdQUvzWiL9UMSCX0oL6uyAGVBwnre6tdPgc9U1g"
   },
   telegram: {
     botToken: "8851734377:AAGW8vdT-WBQ12XJlY7cRCApG1TJXXFPCbE",
