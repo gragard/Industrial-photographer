a7b17cdbb9b7ef010e77516e0787517d48fd93a4
gragard <trakseel0@gmail.com>
2026-07-25 10:49:50 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
-
-
-
-  telegram: {
-    botToken: "8851734377:AAGW8vdT-WBQ12XJlY7cRCApG1TJXXFPCbE",
-    chatIds: ["5934741204", "322615177"]
+window.SITE_CONFIG = {
+  supabase: {
+    url: "https://vynhivvyhzwipkiombxa.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5bmhpdnZ5aHp3aXBraW9tYnhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxOTgwOTksImV4cCI6MjA5OTc3NDA5OX0.YBdxPdQUvzWiL9UMSCX0oL6uyAGVBwnre6tdPgc9U1g"
   }
 };
